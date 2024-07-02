71550066ebd230360920e8d4f434e7080ae5454f
mahmoud-019 <157025626+mahmoud-019@users.noreply.github.com>
2024-07-02 22:16:16 +0300

    Update page.jsx

--- src/app/[locale]/Products/page.jsx
+++ src/app/[locale]/Products/page.jsx
@@ -1,45 +1,22 @@
-import {useTranslations} from 'next-intl';
-import Button from '../components/Button';
-import MotionL from '../layout/MotionL';
-import MotionR from '../layout/MotionR';
-import Box from '../layout/Box';
-import Image from 'next/image';
- 
-export default function Index() {
-  const t = useTranslations('home');
-  return (
+"use client";
+import { useTranslations } from 'next-intl'
+import React from 'react'
+import Box from '@/src/app/layout/Box'
+import ArProduct from "@/src/app/components/ArProduct"
+import EnProduct from '@/src/app/components/EnProduct'
+import { usePathname } from 'next/navigation'
+export default function Page() {
+  const a =useTranslations("pro")
+const path=usePathname()
+ return(
   <>
-
-    <video src="image/promo.mp4" width={1800} height={900} autoPlay loop muted ></video>
+    <div 
+    className='w-full mx-auto rounded-lg text-center prod h-[50svh] mt-14 max-md:mt-20 max-sm:mt-24'>
+  <h1 className='text-3xl bg-clip-text text-transparent bg-gradient-to-r from-red-500 to-green-600 font-bold'>{a("title")}</h1>
+    </div>
     <Box>
-<MotionL>
-  <div className='mt-10 w-11/12 ml-auto  gap-1 p-1 bg-slate-100/50 rounded-md max-md:w-full'>
-  <h2 className='font-bold text-start px-8 text-3xl my-2 '>{t("head1")}</h2>
- <div className='flex  gap-2 max-lg:flex-col'>
-<Image src={t("image1")} width={400} height={200} alt='img' className=' max-lg:w-2/4 max-md:w-full'/>
-  <p>{t("prag1")}</p>
- </div>
-  </div>
-</MotionL>
-
-
-<MotionR>
-<div className='mt-10 w-11/12 mr-auto  gap-1 p-1 bg-slate-100/50 rounded-md max-md:w-full'>
-  <h2 className='font-bold text-end px- text-3xl my-2'>{t("head2")}</h2>
- <div className='flex  gap-2 max-lg:flex-col'>
-  <p>{t("prag2")}</p>
-<Image src={t("image2")} width={400} height={200} alt='img' className=' max-lg:w-2/4 max-md:w-full'/>
- </div>
-  </div>
-</MotionR>
-
-<div className='w-fit mx-auto my-8'>
-  
-  <Button/>
-</div>
-</Box>
- </>
-
-
-  );
+      {(path.includes("/ar"))?<ArProduct/>:<EnProduct/>}
+    </Box>
+  </>
+ )
 }
